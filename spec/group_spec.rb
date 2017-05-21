eb062bb7845db8c95bf63dd9e8288d5ae78b4747
Nacho <nandawtek@gmail.com>
2017-05-21 13:04:44 +0200

    add item form group test

--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 require 'test_support/group'
 
 feature 'Group Service' do
-  scenario 'go to form' do
+  scenario 'goes to form' do
     page = Page::Group.new
     page.go_to_form
     expect(page.has_button_list?).to be true
+  end
+  scenario 'added new group' do
+    page = Page::Group.new
+    page.go_to_form
+    page.fill_form
+    page.save
+    expect(page.has_success_submit?).to be true
   end
 end
--- spec/test_support/group.rb
+++ spec/test_support/group.rb
@@ -8,6 +8,19 @@ module Page
       validate!
     end
 
+    def fill_form
+      sleep(0.5)
+      page.execute_script("document.getElementById('group').textContent='Dishes'");
+    end
+
+    def save
+      find('#submit').click
+    end
+
+    def has_success_submit?
+      has_content?('Dishes')
+    end
+
     def has_button_list?
       has_css?('#groupsList')
     end
